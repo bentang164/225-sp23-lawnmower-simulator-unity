d912e68538e15ba87bd594dc07e4602d0758b212
Zenathano42 <zenathano@gmail.com>
2023-03-24 00:46:16 -0500

    Trying a Sprite-based (rather than Tilemap) approach
    sorry for bad github commit discipline

--- Assets/Scripts/GrassMowing.cs
+++ Assets/Scripts/GrassMowing.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GrassMowing : MonoBehaviour
 {
+    public GameObject mower;
+    SpriteRenderer spriteRenderer;
+
     Texture2D texture;
+
     Color[] colors = new Color[100];
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
         for (int i = 0; i < 100; i++) {
             colors[i] = Color.green;
         }
@@ -17,11 +24,16 @@ public class GrassMowing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rigidbody2D mower = Player.GetComponent<Rigidbody2D>;
+        mower = GameObject.Find("Player");
+        Rigidbody2D mowerBody = mower.GetComponent<Rigidbody2D>();
+        Vector3Int vector = new Vector3Int((int) mowerBody.position.x, (int) mowerBody.position.y, 0);
 
-        texture = this.GetSprite(mower.position);
-        texture.setPixels(mower.position.x, mower.position.y, 10, 10, colors, 0);
+        texture = spriteRenderer.sprite.texture;
+
+        texture.SetPixels((int) mowerBody.position.x, (int) mowerBody.position.y, 10, 10, colors, 0);
 
         texture.Apply(false);
+
+        spriteRenderer.sprite = Sprite.Create(texture, new Rect(), new Vector2(0,0));
     }
 }
