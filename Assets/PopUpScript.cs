9f40f84965f7ba221203f2d993d63f5081d8dc05
JustinChalichemala <jchalich@macalester.edu>
2023-04-04 19:40:16 -0500

    changed active to true

--- Assets/PopUpScript.cs
+++ Assets/PopUpScript.cs
@@ -7,7 +7,7 @@ public class PopUpScript : MonoBehaviour
 {
     public GameObject controlPanel;
     public GameObject XButton;
-    bool active;
+    bool active = true;
     // Start is called before the first frame update
     void Start()
     {
