8157b7f7724efab5a4e0183140968916661133da
JustinChalichemala <jchalich@macalester.edu>
2023-04-06 00:49:54 -0500

    working pause menu. not as aesthetically pleasing but we can change that

---
+++ Assets/PauseMenuScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public string home;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void goHome() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(home);
+    }
+}
