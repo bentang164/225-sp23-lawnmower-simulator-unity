b64265ef46fe826a0187d24504a6c8f82e35079f
JustinChalichemala <jchalich@macalester.edu>
2023-04-09 02:09:52 -0500

    Level One now has an improved UI system. Level 2 is not affected, as are subsequent levels

--- Assets/Scripts/PauseMenuScript.cs
+++ Assets/Scripts/PauseMenuScript.cs
@@ -7,16 +7,30 @@ public class PauseMenuScript : MonoBehaviour
 {
     public GameObject pauseMenu;
     public string home;
+    bool active = false;
+    public GameObject controlPanel;
+    public GameObject XButton;
+
     // Start is called before the first frame update
     void Start()
     {
         pauseMenu.SetActive(false);
+        controlPanel.SetActive(false);
+        XButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            Time.timeScale = 0f;
+            if (controlPanel.activeSelf && XButton.activeSelf) {
+                controlPanel.SetActive(!controlPanel.activeSelf);
+                XButton.SetActive(!XButton.activeSelf);
+                active = false;
+            }
+        }
     }
 
     public void Pause() {
@@ -32,5 +46,21 @@ public class PauseMenuScript : MonoBehaviour
     public void goHome() {
         Time.timeScale = 1f;
         SceneManager.LoadScene(home);
+    }
+
+
+    public void OpenClosePopUp() {
+        if (!active) {
+            controlPanel.SetActive(true);
+            XButton.SetActive(true);
+            active = true;
+            pauseMenu.SetActive(false);
+        } else {
+            controlPanel.SetActive(false);
+            XButton.SetActive(false);
+            pauseMenu.SetActive(true);
+            active = false;
+
+        }
     }
 }
