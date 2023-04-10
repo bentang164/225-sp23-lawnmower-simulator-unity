893f0ce997cfb085cf30e45bd874b17a34b7f487
Ben Tang <40529554+bentang164@users.noreply.github.com>
2023-04-10 11:44:00 -0500

    Added path around grass, updated trail renderer
    Potential solution for not being able to hit the corners

--- Assets/Scripts/DetectCompletion.cs
+++ Assets/Scripts/DetectCompletion.cs
@@ -7,14 +7,14 @@ public class DetectCompletion : MonoBehaviour
     private GameObject player = null;
     private int mowedTiles = 0;
     
-    private int NUM_TILES = 10500; // 150x70 tilemap.
+    private int NUM_TILES = 11200; // 160x70 tilemap.
     private readonly int DEFAULT_Z = 0;
 
     [SerializeField]
     private Tilemap completionTilemap;
 
     [SerializeField]
-    private int threshold; // Default: 90% of NUM_TILES, 8650 for level 2
+    private int threshold; // Default: 90% of NUM_TILES, 8415 for level 2, 
 
     //Start is called before the first frame update
     void Start()
@@ -37,52 +37,11 @@ public class DetectCompletion : MonoBehaviour
         float playerRawX = player.transform.position.x;
         float playerRawY = player.transform.position.y;
 
-        //Mow(new Vector3Int((int) (playerRawX - 2 * Mathf.Cos(player.GetComponent<Rigidbody2D>().rotation)), (int) (playerRawY - 2 * Mathf.Sin(player.GetComponent<Rigidbody2D>().rotation)), DEFAULT_Z));
-        //Mow(new Vector3Int((int) (playerRawX - 1 * Mathf.Cos(player.GetComponent<Rigidbody2D>().rotation)), (int) (playerRawY - 1 * Mathf.Sin(player.GetComponent<Rigidbody2D>().rotation)), DEFAULT_Z));
-        //Mow(new Vector3Int((int)playerRawX, (int)playerRawY, DEFAULT_Z));
-        //Mow(new Vector3Int((int) (playerRawX + 1 * Mathf.Cos(player.GetComponent<Rigidbody2D>().rotation)), (int) (playerRawY + 1 * Mathf.Sin(player.GetComponent<Rigidbody2D>().rotation)), DEFAULT_Z));
-        //Mow(new Vector3Int((int) (playerRawX + 2 * Mathf.Cos(player.GetComponent<Rigidbody2D>().rotation)), (int) (playerRawY + 2 * Mathf.Sin(player.GetComponent<Rigidbody2D>().rotation)), DEFAULT_Z));
-
-        //Mow(new Vector3Int(Mathf.RoundToInt(playerRawX - 2), Mathf.RoundToInt(playerRawY - 2), DEFAULT_Z));
-        //Mow(new Vector3Int(Mathf.RoundToInt(playerRawX - 1), Mathf.RoundToInt(playerRawY - 1), DEFAULT_Z));
-        //Mow(new Vector3Int(Mathf.RoundToInt(playerRawX), Mathf.RoundToInt(playerRawY), DEFAULT_Z));
-        //Mow(new Vector3Int(Mathf.RoundToInt(playerRawX + 1), Mathf.RoundToInt(playerRawY + 1), DEFAULT_Z));
-        //Mow(new Vector3Int(Mathf.RoundToInt(playerRawX + 2), Mathf.RoundToInt(playerRawY + 2), DEFAULT_Z));
-
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 Mow(new Vector3Int(Mathf.RoundToInt(playerRawX - 2 + i), Mathf.RoundToInt(playerRawY - 2 + j), DEFAULT_Z));
             }
         }
-
-        /*
-        if (Time.frameCount % 180 == 0) {
-            print("left side raw: " + (playerRawX - 2) + ", " + (playerRawY - 2));
-            //print("left side int: " + (int)(playerRawX - 2) + ", " + (int)(playerRawY - 2));
-            //print("right side int: " + (int)(playerRawX + 2) + ", " + (int)(playerRawY+ 2));
-            print("right side raw: " + (playerRawX + 2) + ", " + (playerRawY + 2));
-
-            print("center " + playerRawX + ", " + playerRawY);
-
-        }
-        */
-
-        // Update the completion tilemap at the player's x and y coordinates, based on position.
-        // This will NOT WORK if the size of the tilemap is changed from default scale!
-        //if (playerRawY < 0 && playerRawX < 0)
-        //{
-        //    Mow(new Vector3Int((int) playerRawX - 1, (int) playerRawY - 1, DEFAULT_Z));
-        //} else if (playerRawY > 0 && playerRawX < 0)
-        //{
-        //    Mow(new Vector3Int((int) playerRawX - 1, (int) playerRawY, DEFAULT_Z));
-        //} else if (playerRawY < 0 && playerRawX > 0)
-        //{
-        //    Mow(new Vector3Int((int) playerRawX, (int) playerRawY - 1, DEFAULT_Z));
-        //}
-        //else
-        //{
-        //    Mow(new Vector3Int((int) playerRawX, (int) playerRawY, DEFAULT_Z));
-        //}
     }
 
     public void Mow(Vector3Int position)
@@ -94,8 +53,6 @@ public class DetectCompletion : MonoBehaviour
             // To disable, remove the below line.
             completionTilemap.SetTile(position, null);
             mowedTiles++;
-
-            //print(mowedTiles);
         }
     }
 }
